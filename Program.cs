9602ecac7146c6e0de8f05e0940ad89db176a28d
Andile <s224097083@mandela.ac.za>
2024-02-20 00:31:31 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -14,51 +14,78 @@ namespace loadshedding
         {
             while (true)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write("Welcome.\n" +
                     "1. Check Allowance\n" +
                     "2. Check Todays Schedule\n" +
-                    "3. Coming soon\n");
-                await Today();
-                Console.ReadLine();
+                    "3. Load shedding stages\n" +
+                    "Choice: ");
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        await CheckAllowance();
+                    break;
+                    case 2:
+                        await Today();
+                    break;
+                    case 3:
+                        await CheckStage();
+                    break;
+                    default:
+                        Console.WriteLine("Invalid Selection");
+                        break;
+                }
+                //await Today();
+                //Console.ReadLine();
             }
             
             
         }
+        public static async Task CheckStage()
+        {
+            
+            string responseBody = await GetData("https://developer.sepush.co.za/business/2.0/status");
+            StatusClass message = JsonSerializer.Deserialize<StatusClass>(responseBody);
+            if (message == null)
+            {
+                Console.WriteLine("Failed");
+            }
+            else
+            {
+                Console.WriteLine($"Current And Future Stages\n" +
+                    $"Current Stage: {message.status.eskom.stage} Last Updated: {message.status.eskom.stage_updated.ToShortDateString()} {message.status.eskom.stage_updated.ToShortTimeString()}\n");
+
+                Console.WriteLine("\nFuture stages implantations\n");
+                if (message.status.eskom.next_stages.Length == 0)
+                {
+                    Console.WriteLine("No future updates");
+
+                }
+                else
+                {
+
+                    foreach (var item in message.status.eskom.next_stages)
+                    {
+
+                        Console.WriteLine($"Stage: {item.stage}\nStart Time: {item.stage_start_timestamp.ToShortDateString()} {item.stage_start_timestamp.ToShortTimeString()}\n\n");
+                    }
+                }
+                
+            }
+        }
         public static async Task CheckAllowance()
         {
-            using (HttpClient client = new HttpClient())
+            
+            string responseBody = await GetData("https://developer.sepush.co.za/business/2.0/api_allowance");
+            Root message = JsonSerializer.Deserialize<Root>(responseBody);
+            if (message == null)
             {
-                try
-                {
-                    // Define the API endpoint URL
-                    string apiUrl = "https://developer.sepush.co.za/business/2.0/api_allowance";
-                    
-                    client.DefaultRequestHeaders.Add("token", "2CFFCF5B-FFDD4CD8-B3EC867E-F5CECE38");
-
-                    // Make a GET request to the API
-                    HttpResponseMessage response = await client.GetAsync(apiUrl);
-
-                    // Ensure the response is successful
-                    response.EnsureSuccessStatusCode();
-
-                    // Read the response content as a JSON string
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    Root message = JsonSerializer.Deserialize<Root>(responseBody);
-                    if (message == null)
-                    {
-                        Console.WriteLine("Failed");
-
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Used: {message.allowance.count}\nLimit: {message.allowance.limit}\nType: {message.allowance.type}");
-                    }
-                }
-                catch (HttpRequestException ex)
-                {
-                    // Handle any errors with the API request
-                    Console.WriteLine($"Error: {ex.Message}");
-                }
+                Console.WriteLine("Failed");
+            }
+            else
+            {
+                Console.WriteLine($"Used: {message.allowance.count}\nLimit: {message.allowance.limit}\nType: {message.allowance.type}");
             }
         }
         public static async Task Today()
@@ -75,22 +102,24 @@ namespace loadshedding
                 Console.WriteLine($"Load shedding for: {message.info.name} \n {message.info.region}\n Amount of Events: {message.events.Length}\n\n" +
                     $"");
                 int x = 1;
+
                 foreach (var item in message.events)
                 {
                     Console.WriteLine($"Event: {x}\nStage : {item.note}\n" +
-                        $"Date: {item.start.ToString("dddd d MMMM yyyy")}\n" +
-                        $"Start Time: {item.start.ToString("t")}\n\n" +
-                        $"End Time: {item.end.ToString("t")}\n\n");
+                        $"Date: {item.start:dddd d MMMM yyyy}\n" +
+                        $"Start Time: {item.start:t}\n\n" +
+                        $"End Time: {item.end:t}\n\n");
                     x++;
                 }
 
-
-                //Console.WriteLine($"Source: {message.schedule.source}");
-
-                //Console.WriteLine(responseBody);
-
-
-
+                Console.WriteLine($"================ NOTICE !!! =====================\n" +
+                    $"== Stage : {message.events[0].note}\t\t\t\t=\n" +
+                        $"== Date: {message.events[0].start:dddd d MMMM yyyy}\t\t=\n" +
+                        $"== Start Time: {message.events[0].start:t}\t\t\t\t=\n" +
+                        $"== End Time: {message.events[0].end:t}\t\t\t\t=\n" +
+                        $"================ NOTICE !!! =====================\n\n");
+                var diff = message.events[0].start - DateTime.Now;
+                Console.WriteLine($" Total Minutes till LOAD SHEDDING: {diff.TotalMinutes:f0}");
             }
             if (false)
             {
@@ -153,6 +182,41 @@ namespace loadshedding
                 }
             }
            
+        }
+        public static async Task Shutdown(Rootobject rootobject)
+        {
+            string i = await GetData("https://developer.sepush.co.za/business/2.0/area?id=nelsonmandelabay-5-summerstranduptomarinehotelarea8");
+            Rootobject message = JsonSerializer.Deserialize<Rootobject>(i);
+            if (message.events[0].start.Date == DateTime.Now.Date)
+            {
+                Console.WriteLine($"================ NOTICE !!! =====================\n" +
+                    $"== Stage : {message.events[0].note}\t\t\t\t=\n" +
+                        $"== Date: {message.events[0].start:dddd d MMMM yyyy}\t\t=\n" +
+                        $"== Start Time: {message.events[0].start:t}\t\t\t\t=\n" +
+                        $"== End Time: {message.events[0].end:t}\t\t\t\t=\n" +
+                        $"================ NOTICE !!! =====================\n\n");
+                var diff = message.events[0].start - DateTime.Now;
+                switch (diff.TotalMinutes)
+                {
+                    case 30:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("");
+
+                        break;
+                    case 20:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 10:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                }
+            }
         }
         public static async Task<string> GetData(string apiUrl)
         {
